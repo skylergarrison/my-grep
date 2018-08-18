35737e625a260200b2f6b68af8c614160000c754
WSG <skyler.garrison@gmail.com>
2018-08-18 02:25:56 -0400

    Source code
    Added source file for my-grep

---
+++ mygrep.pl
@@ -0,0 +1,14 @@
+# mygrep.pl
+#! /usr/local/bin/perl
+
+die "Usage mygrep string \n" if @ARGV < 1;
+use strict;
+my $string = shift;
+my $ct = 0;
+
+while (<>) {
+    $ct++;
+    print "$ct:\t$_" if /$string/;
+}
+exit;
+
